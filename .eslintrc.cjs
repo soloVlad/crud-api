3410341184ab652bb95eb7cc93b5027138172b7f
Vlad Solovey <connectwithvlad@gmail.com>
2024-02-06 23:51:13 +0300

    style: change eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,71 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'rules': {
+    "no-unused-vars": "warn",
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'eol-last': [
+      'error',
+      'always'
+    ],
+    'comma-dangle': ['error', {
+      'arrays': 'always-multiline',
+      'objects': 'always-multiline',
+      'imports': 'always-multiline',
+      'exports': 'always-multiline',
+      'functions': 'never',
+    }],
+    'max-len': ['warn', {
+      'code': 100,
+      'ignoreStrings': true,
+      'ignoreUrls': true,
+    }],
+  },
+  "ignorePatterns": [
+    ".eslintrc.cjs",
+    "node_modules",
+    "dist"
+  ],
+};
